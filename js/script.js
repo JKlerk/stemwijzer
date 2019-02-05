700f3b5bd3bf2b3bb7ea5fb6446c4c166205bacb
JKlerk <joeydutchman@gmail.com>
2019-02-05 14:28:11 +0100

    Added data.js

--- js/script.js
+++ js/script.js
@@ -3,28 +3,24 @@ var desc = document.getElementById("desc")
 var footer = document.getElementById("footer")
 var q = document.getElementById("question")
 
-var quest = [
-	"Stelling 1",
-	"Stelling 2",
-	"Stelling 3",
-	"Stelling 4",
-	"Stelling 5",
-	"Stelling 6",
-	"Stelling 7",
-];
 
-var ra  = Math.floor(Math.random() * quest.length);
+var random  = Math.floor(Math.random() * subjects.length);
+console.log(random)
+console.log(subjects[0])
 
 function start(){
 	console.log("Started")
 	titles.style.display = "none";
 	desc.style.display = "none";
 	footer.style.display = "none";
-	questions();
+	stelling();
 }
 
-function questions(){
-	q.innerHTML = "1." + quest[ra];
+function stelling(){
+	q.innerHTML = subjects["title"];
+
+	var object = Object.values(subjects);
+	console.log(object['title']);
 }
 
 function reply_click(clicked_id){
@@ -32,3 +28,4 @@ function reply_click(clicked_id){
 	console.log(clicked_id)
     clicked_id.classList.add("w3-teal");;
 }
+
