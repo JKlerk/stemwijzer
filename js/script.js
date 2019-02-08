ae921a64111e013a7231979173f55b38cfb2f5af
JKlerk <joeydutchman@gmail.com>
2019-02-08 14:41:58 +0100

    Update js

--- js/script.js
+++ js/script.js
@@ -5,65 +5,80 @@ var stel = document.getElementById("stelling");
 var q = document.getElementById("question");
 var button = document.getElementById("button");
 var back = document.getElementById("back");
-qcount = 1;
-console.log(subjects[0])
 
-//Start function
+var answers = [];
+var buttonStart = document.getElementById("start")
 
-function start(){
+buttonStart.addEventListener("click", function start(){
 	console.log("Started")
 	titles.style.display = "none";
 	desc.style.display = "none";
 	footer.style.display = "none";
 	button.classList.remove('w3-hide');
 	q.classList.remove('w3-hide')
+	document.getElementById('animate').classList.add('w3-animate-right');
 	back.classList.remove('w3-hide');
 	document.getElementById("container").classList.remove('mt-4')
 	q.style.marginBottom = "100px";
 	stelling();
-}
+})
+
+qcount = 0;
+// console.log(subjects[0])
+
+//Verandering Vraag
 
 function stelling(){
-	removeColor();
-	if (qcount == 7){
+	
+	if (qcount == subjects.length){
 		q.innerHTML = "Einde";
 		stel.innerHTML = "Einde";
+		back.classList.add('w3-hide');
+		document.getElementById("container").classList.add('mt-4')
 		for (var i = 1; i <= 4; i++) {
 			document.getElementById(i).classList.add("w3-hide");
 		}
 	} else{
-		var subjectTitle = subjects.map(function(subjects) {
-  			return subjects['title'];
-		});
-		var subjectStatement = subjects.map(function(subjects) {
-	  		return subjects['statement'];
-		});
-		q.innerHTML = subjectStatement[qcount];
-		stel.innerHTML = qcount + ". " + subjectTitle[qcount];
-		console.log(subjectTitle[qcount]);
+		stel.innerHTML = qcount+1 + ". " + subjects[qcount].title;
+		q.innerHTML = subjects[qcount].statement;
+	}
+	console.log(answers)
+}
+
+function goBack(){
+	if (qcount != 0){
+		qcount--
+		stelling();
+		for (var i = 1; i < 3; i++) {
+			document.getElementById(i).classList.remove('w3-teal');
+			document.getElementById(i).classList.add('w3-black');	
+		}
+
+		document.getElementById(answers[qcount]).classList.add('w3-teal');
+		document.getElementById(answers[qcount]).classList.remove('w3-black');
+
+	} else{
+		location.reload();
 	}
 }
+
+//Knoppen
 
 function reply_click(clicked_id){
 	var buttonid = document.getElementById(clicked_id)
 
-	if (buttonid.classList.contains("w3-teal")){
-		alert ("Already selected");	
-	} else{
-		removeColor();
-		// alert (clicked_id)	
-		buttonid.classList.remove("w3-black");
-    	buttonid.classList.add("w3-teal");
-    	qcount++;
-    	stelling();
-    	console.log(qcount)
+	answers[qcount] = clicked_id;
+	for (var i = 1; i < 3; i++) {
+		document.getElementById(i).classList.remove('w3-teal');
+		document.getElementById(i).classList.add('w3-black');	
 	}
+
+	qcount++;
+	if (answers.length > qcount){
+		document.getElementById(answers[qcount]).classList.add('w3-teal');
+		document.getElementById(answers[qcount]).classList.remove('w3-black');
+	}
+	stelling();
 }
 
-function removeColor(){
-	for (var i = 1; i <= 3; i++) {
-		document.getElementById(i).classList.remove("w3-teal");
-		document.getElementById(i).classList.add("w3-black");
-	}
-}
 
