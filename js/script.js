45ab7c7fc5c2d4c4d2cf741e74a0520178682fc3
JKlerk <99045878@mydavinci.nl>
2020-01-18 03:19:40 +0100

    Start of calculation

--- js/script.js
+++ js/script.js
@@ -4,7 +4,11 @@ const stellingParties = document.getElementById('parties');
 const buttons = document.getElementById('buttons');
 const buttonBack = document.getElementById('buttonBack');
 const multiply = document.getElementById('multiply');
+
 // Answers
+var pro = [];
+var contra = [];
+var ambivalent = [];
 var answers = [];
 
 // Bar length
@@ -31,19 +35,22 @@ document.getElementById("start").addEventListener("click", function start(){
 
 // Sets stelling on page
 function getStelling(){
-	console.log(answers)
-	stellingTitle.innerHTML = questionCount + 1 + '. ' + subjects[questionCount].title
-	stellingStatement.innerHTML = subjects[questionCount].statement
-	document.getElementById("bar").style.width = curLength + "%";
-	subjects[questionCount].parties.forEach(function(element) {
-		if (element.position == "pro") {
-			document.getElementById("eens").innerHTML += "<details class=\"opinion__party\"><summary class=\"party__title\">" + element.name + "</summary><p class=\"party__description\"> " + element.explanation + "</p></details>"
-		} else if (element.position == "ambivalent") {
-			document.getElementById("none").innerHTML += "<details class=\"opinion__party\"><summary class=\"party__title\">" + element.name + "</summary><p class=\"party__description\"> " + element.explanation + "</p></details>"
-		} else if (element.position == "contra") {
-			document.getElementById("oneens").innerHTML += "<details class=\"opinion__party\"><summary class=\"party__title\">" + element.name + "</summary><p class=\"party__description\"> " + element.explanation + "</p></details>"
-		}
-	});
+	if(answers.length !== 7){
+		stellingTitle.innerHTML = questionCount + 1 + '. ' + subjects[questionCount].title
+		stellingStatement.innerHTML = subjects[questionCount].statement
+		document.getElementById("bar").style.width = curLength + "%";
+		subjects[questionCount].parties.forEach(function(element) {
+			if (element.position == "pro") {
+				document.getElementById("eens").innerHTML += "<details class=\"opinion__party\"><summary class=\"party__title\">" + element.name + "</summary><p class=\"party__description\"> " + element.explanation + "</p></details>"
+			} else if (element.position == "ambivalent") {
+				document.getElementById("none").innerHTML += "<details class=\"opinion__party\"><summary class=\"party__title\">" + element.name + "</summary><p class=\"party__description\"> " + element.explanation + "</p></details>"
+			} else if (element.position == "contra") {
+				document.getElementById("oneens").innerHTML += "<details class=\"opinion__party\"><summary class=\"party__title\">" + element.name + "</summary><p class=\"party__description\"> " + element.explanation + "</p></details>"
+			}
+		});
+	} else{
+		calculate();
+	}
 }
 
 function changeStelling(opinion){
@@ -77,4 +84,39 @@ function goBack(){
 		questionCount--;	
 		getStelling();
 	}
+}
+
+
+// Calculate
+function calculate(){
+	// Haalt alle antwoord op index = question nummer
+	for (let index = 0; index < answers.length; index++) {
+		var yourOpinions = answers[index].opinion
+		// Haalt alle parties op. logged elke keer voor elke vraag opnieuw de positions van de partijen
+		for (let i = 0; i < subjects[index].parties.length; i++) {
+			var party = subjects[index].parties[i]
+			console.log('Question: ' + index)
+			console.log('Party:' + party.name)
+			console.log('Position:' + party.position)
+			// Geeft punten als mening zelfde is als party
+			if(party.position === 'pro' && yourOpinions){
+				if (isNaN(party.points)) party.points = 0
+				console.log(party.points += 1);
+			} else{
+				if (isNaN(party.points)) party.points = 0
+				console.log(party.points -= 1);
+			}
+		}
+	}
+}
+
+// Shows end result
+function showResult(){
+	var div = document.getElementById("results")
+	var result = document.createElement("span")
+
+	for (let index = 0; index < subjects.length; index++) {
+		result.innerHTML += '<p class="mb-0 mt-1">' + parties[i].name + '</p>' + '<div class="w3-'+ color +'" style="max-width:1000px;width:'+ calcLength +'%">'+ status + percent + '%</div>';
+	}
+	div.appendChild(result)
 }
